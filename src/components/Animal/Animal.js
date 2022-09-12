10f06226893be39256fa4503813f9e602f25f10d
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-12 15:12:56 -0700

    animal details rendering on page as well as background img'

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,11 @@
-export default function Animal() {
-  return <></>;
+export default function Animal(animal) {
+  return <>
+    <div>
+    name: {animal.name}
+    type: {animal.type}
+    says: {animal.says}
+      <img src={`{${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
+    </div>
+  
+  </>;
 }
