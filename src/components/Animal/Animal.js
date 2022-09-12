841f6fc0148ff1205873a1dd1f046334a243ef02
Tanner Richards <Tanner.richards990@gmail.com>
2022-09-12 16:29:31 -0700

    messed with layout

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -4,8 +4,7 @@ import './Animal.css';
 export default function Animal(props) {
   return <>
     <div className="animal" style={{ top: props.animal.top, left: props.animal.left }}>
-      {props.animal.name}
-      {props.animal.type}
+      <p className="name">{props.animal.name}</p>
       {props.animal.says}
       <img src={`${process.env.PUBLIC_URL}/animals/${props.animal.type}.svg`} />
     </div>
